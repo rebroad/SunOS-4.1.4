255c27cc644dd9145df70ecd46c2e9fffa907da5
R E Broadley <rebroad@users.noreply.github.com>
2026-09-17 21:31:28 +0100

    build: omit unavailable symbol reorderer

--- tools/build-sun4m-idle.sh
+++ tools/build-sun4m-idle.sh
@@ -56,6 +56,7 @@ sed -i -E "s/(fd_asm|sr|st_conf|st)\\.(o|L) //g" Makefile
 # OMAGIC/non-page-aligned link mode for this kernel.
 sed -i "s/ -p / /" Makefile
 sed -i "s/-T F0004000/-Ttext 0xF0004000/" Makefile
+sed -i "/@symorder /d" Makefile
 
 if ! make -j1 all \
     CC="sparc64-linux-gnu-gcc -std=gnu89 -fno-builtin -fcommon -m32 -mno-v8plus -mcpu=v8 -fno-pie -fleading-underscore -Dsparc -Dsun -Uunix -Wno-endif-labels -Wno-implicit-int -Wno-implicit-function-declaration -Wno-return-type" \
