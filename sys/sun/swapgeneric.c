416b0a361691e6364344c877d09bcad0e9ed6dd4
R E Broadley <rebroad@users.noreply.github.com>
2026-09-17 20:46:15 +0100

    build: declare generic swap helpers

--- sys/sun/swapgeneric.c
+++ sys/sun/swapgeneric.c
@@ -58,6 +58,9 @@ static int bootpath_debug = 1;		/* Non-zero for debug messages */
 dev_t	rootdev;
 struct vnode *rootvp;
 
+static int chkopenable();
+static int build_bdevlist();
+
 /*
  * Flags in bdevlist/cdevlist.
  * This stuff moved outside the "ifdef OPENPROM" for building
@@ -1115,7 +1118,7 @@ found:
 /*
  * check if dev is openable, 0 no, 1 yes
  */
-static
+static int
 chkopenable(dev)
 	dev_t	dev;
 {
